1889211e580cedc11c816609aa7541675e6fe2a4
Esteban <estebamlo1975@gmail.com>
2022-02-21 22:55:29 -0300

    Giving style to pages

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,19 @@
-console.log(document.body);
-console.log(document.head);
+//Document
+const d = document,
+  hamburgerBtn = d.querySelector(".hamburger"),
+  hiddenMenu = d.querySelector(".hidden-menu");
+
+//Creating function to make hidden menu show up
+d.addEventListener("click", (e) => {
+  let target = e.target;
+  if (target === hamburgerBtn || target.matches(".hamburger i")) {
+    e.preventDefault();
+    hiddenMenu.classList.add("animate__animated", "animate__fadeInLeft");
+  }
+});
+
+window.addEventListener("resize", (e) => {
+  if (window.innerWidth > 658) {
+    hiddenMenu.classList.remove("animate__animated", "animate__fadeInLeft");
+  }
+});
